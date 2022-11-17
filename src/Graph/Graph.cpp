306c9c59692ba5e4c149cc8faa8ec5521b80bc1c
deturpant <vlad3032.kozlov@yandex.ru>
2022-11-17 20:18:34 +0300

    Add custom constructor

--- src/Graph/Graph.cpp
+++ src/Graph/Graph.cpp
@@ -14,4 +14,14 @@ namespace KVA {
             }
         }
     }
+
+    Graph::Graph(int _n) {
+        this->matrix_size = _n;
+        for (int i = 0; i < this->matrix_size; i++) {
+            for (int j = 0; j < this->matrix_size; j++) {
+                this->matrix[i][j] = 0;
+                this->matrix[j][i] = this->matrix[i][j];
+            }
+        }
+    }
 } // KVA
