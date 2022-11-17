5dc2a681b29ac2d4aeaa7f01a035099ebacf539a
deturpant <vlad3032.kozlov@yandex.ru>
2022-11-17 20:22:15 +0300

    add showMatrix

--- src/Graph/Graph.cpp
+++ src/Graph/Graph.cpp
@@ -2,13 +2,15 @@
 // Created by deturpant on 17.11.22.
 //
 
+#include <cstdlib>
+#include <iostream>
 #include "Graph.h"
 
 namespace KVA {
     Graph::Graph() {
         this->matrix_size = 7;
         for (int i = 0; i < this->matrix_size; i++) {
-            for (int j = 0; j< this->matrix_size; j++) {
+            for (int j = 0; j < this->matrix_size; j++) {
                 this->matrix[i][j] = 0;
                 this->matrix[j][i] = this->matrix[i][j];
             }
@@ -31,5 +33,16 @@ namespace KVA {
 
     void Graph::setMatrix(int _n) {
         this->matrix_size = _n;
+    }
+
+    void Graph::ShowMatrix() {
+        system("cls");
+        std::cout << "Матрица смежности:\n";
+        for (int i = 0; i < matrix_size; i++) {
+            for (int j = 0; j < matrix_size; j++) {
+                std::cout << " " << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 } // KVA
